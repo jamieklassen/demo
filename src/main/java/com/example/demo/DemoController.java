0d67e4619594c5877e71479aba38aba682a7e4d0
Jamie Klassen <jamie.klassen@cgi.com>
2018-01-03 10:25:01 -0500

    add some persistence to allow for nontrivial performance tests

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -2,12 +2,20 @@ package com.example.demo;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class DemoController {
-	@RequestMapping("/")
-	public @ResponseBody String hello() {
-		return "Hello Mark\n";
-	}
+
+    private String name = "World";
+
+    @RequestMapping("/")
+    public @ResponseBody
+    String hello(@RequestParam(required = false) String name) {
+        if (name != null) {
+            this.name = name;
+        }
+        return "Hello " + this.name + "\n";
+    }
 }
