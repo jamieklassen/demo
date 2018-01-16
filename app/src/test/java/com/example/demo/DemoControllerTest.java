3eef01133be40620f71db841002c6a1abc69e7cf
Jamie Klassen <jamie.klassen@cgi.com>
2018-01-16 12:51:52 -0500

    remove test for simpler functionality

--- app/src/test/java/com/example/demo/DemoControllerTest.java
+++ app/src/test/java/com/example/demo/DemoControllerTest.java
@@ -7,13 +7,6 @@ import static org.junit.Assert.*;
 public class DemoControllerTest {
 
     @Test
-    public void helloPersistsName() {
-        DemoController controller = new DemoController();
-        controller.hello("Mark");
-        assertEquals("Hello Mark\n", controller.hello(null));
-    }
-
-    @Test
     public void helloReturnsDefaultName() {
         DemoController controller = new DemoController();
         assertEquals("Hello World\n", controller.hello(null));
