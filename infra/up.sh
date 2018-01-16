f7e210eb380d6ada51d76f8c487f2ec8c501676d
Jamie Klassen <jamie.klassen@cgi.com>
2018-01-16 12:13:01 -0500

    cleaning up gogs/jenkins setup

--- infra/up.sh
+++ infra/up.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
-tmpdir=/tmp/app
-rm -rf $tmpdir
-mkdir -p $tmpdir
-git -C $tmpdir init
+tmpdir=$1
 shopt -s dotglob
 cp -r ../app/* $tmpdir/
 shopt -u dotglob
-git -C $tmpdir add $tmpdir
-git -C $tmpdir ci -m 'initial commit'
-git -C $tmpdir config receive.denyCurrentBranch updateInstead
-GIT_DIR=$tmpdir docker-compose up -d
