68b6193a5aa36d485d7b707c358df0d32d59fa6d
Jamie Klassen <jamie.klassen@cgi.com>
2018-01-17 14:19:41 -0500

    use chef for app servers
    some weirdness around permissions on the /var/lib/tomcat7/webapps folder
    related to #2

---
+++ infra/jenkins/knife.rb
@@ -0,0 +1,6 @@
+node_name                ''
+client_key               '/var/jenkins_home/.chef/.pem'
+validation_client_name   'chef-validator'
+validation_key           '/etc/chef-server/chef-validator.pem'
+chef_server_url          'http://chef-server:8889'
+syntax_check_cache_path  '/var/jenkins_home/.chef/syntax_check_cache'
