2efcb10cd6afa5cf9b5ad223f88cb9e3cbf2aaac
Jamie Klassen <jamie.klassen@cgi.com>
2018-02-21 15:19:36 -0500

    clean up

--- app/src/main/java/com/example/demo/MonitoringConfig.java
+++ app/src/main/java/com/example/demo/MonitoringConfig.java
@@ -4,7 +4,6 @@ import io.prometheus.client.exporter.MetricsServlet;
 import io.prometheus.client.hotspot.DefaultExports;
 import io.prometheus.client.spring.boot.SpringBootMetricsCollector;
 import org.flywaydb.core.Flyway;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.actuate.endpoint.PublicMetrics;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
@@ -15,7 +14,7 @@ import java.util.Collection;
 
 @Configuration
 class MonitoringConfig {
-   
+
     @Bean
     Flyway flyway(DataSource dataSource) {
         Flyway flyway = new Flyway();
@@ -26,10 +25,8 @@ class MonitoringConfig {
 
     @Bean
     SpringBootMetricsCollector springBootMetricsCollector(Collection<PublicMetrics> publicMetrics) {
-
         SpringBootMetricsCollector springBootMetricsCollector = new SpringBootMetricsCollector(publicMetrics);
         springBootMetricsCollector.register();
-
         return springBootMetricsCollector;
     }
 
